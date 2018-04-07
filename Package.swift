893d755d601f36efdeeae8ea2a96c6572d20a57d
Adrian Ensan <adrianensan@gmail.com>
2018-04-07 03:17:12 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "swift-server",
+    targets: [
+        .target(
+            name: "swift-server",
+            dependencies: []),
+    ]
+)
